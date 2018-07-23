08fe06126422500da67311d1059e52dc4f0d3708
Barbara <barbarac2339@gmail.com>
2018-07-23 14:46:18 -0500

    added tags

--- WeatherOrNot/main.py
+++ WeatherOrNot/main.py
@@ -30,6 +30,42 @@ class AddClothingHandler(webapp2.RequestHandler):
         response_html = jinja_env.get_template("templates/upload-images/index.html")
         self.response.headers['Content-Type'] = 'text/html'
         return self.response.write(response_html.render())
+        self.response.headers['Content-Type'] = 'text/html'
+        response_html = jinja_env.get_template('templates/calc.html')
+        length = self.request.get('length')
+        type=self.request.get('type')
+        material = self.request.get('materials')
+        if type == 'dress':
+
+        elif type=='skirt':
+
+        elif type=='shirt':
+
+        elif type=='pants':
+
+        if length=='short':
+
+        elif length=='long':
+
+        elif length=='three_quarters':
+
+        if material == 'Cotton':
+
+        elif material=='Nylon':
+
+        elif material=='Spandex':
+
+        elif material=='Wool':
+
+        elif material == 'Polyester':
+        #result = int(number1) + int(number2)
+        #self.response.write(response_html % (result, number1, number2))
+        values = {
+            "tempResult": result,
+            "tempNumber1": number1,
+            "tempNumber2": number2,
+        }
+        self.response.write(response_html.render(values))
     def post(self):
         requestUrl = self.request.get('url')
         logging.info('server saw a request to add %s to list of favorites' % (requestUrl))
