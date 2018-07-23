73f231aaaf12106bfdc77ee570f541dc56ec79ac
Barbara <barbarac2339@gmail.com>
2018-07-23 09:57:08 -0500

    m

---
+++ WeatherOrNot/Imgur-Upload-master/js/upload.js
@@ -0,0 +1,13 @@
+var feedback = function(res) {
+    if (res.success === true) {
+        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
+        document.querySelector('.status').classList.add('bg-success');
+        document.querySelector('.status').innerHTML =
+            'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+    }
+};
+
+new Imgur({
+    clientid: 'ea32dd4d0404854', //You can change this ClientID
+    callback: feedback
+});
