7d90af7707a9513b71746ffdd8e4cd52f06b5e75
Barbara <barbarac2339@gmail.com>
2018-07-24 15:11:59 -0500

    uh

--- WeatherOrNot/main.py
+++ WeatherOrNot/main.py
@@ -50,16 +50,24 @@ class AddClothingHandler(webapp2.RequestHandler):
         response_html = jinja_env.get_template('templates/upload-images/index.html')
         logging.info('server saw a request to add %s to list of favorites' % (requestUrl))
 
+
 class SuggestionsHandler(webapp2.RequestHandler):
     def get(self):
         response_html = jinja_env.get_template("templates/suggestions_page/suggestions.html")
-
+        weather_response_html = jinja_env.get_template("templates/weather-test.html")
+        temp = self.request.get("temp")
+        weather = self.request.get("condition")
+        logging.info("The weather is: " + weather)
+        length_cloth=WardrobeSave.length=="length"
+        if (weather=="sunny"):
+            length_cloth=WardrobeSave.length=="short"
         values={
-            "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt").fetch(),
-            "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants").fetch(),
-            "skirtWardrobe":WardrobeSave.query(WardrobeSave.type=="skirt").fetch(),
-            "dressWardrobe":WardrobeSave.query(WardrobeSave.type=="dress").fetch(),
+            "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", length_cloth).fetch(),
+            "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", length_cloth).fetch(),
+            "skirtWardrobe":WardrobeSave.query(WardrobeSave.type=="skirt", length_cloth).fetch(),
+            "dressWardrobe":WardrobeSave.query(WardrobeSave.type=="dress", length_cloth).fetch(),
         }
+        self.response.write(weather_response_html.render())
         self.response.write(response_html.render(values))
 
 
