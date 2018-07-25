29a041c6534efb5161563e4f062095e8b6095966
William Flowers <waffy1102@gmail.com>
2018-07-25 10:55:42 -0500

    Added commas cause barbara forgot them

--- WeatherOrNot/main.py
+++ WeatherOrNot/main.py
@@ -75,7 +75,9 @@ class SuggestionsHandler(webapp2.RequestHandler):
 
         response_html = jinja_env.get_template("templates/suggestions_page/suggestions.html")
         values={
-            "topsWardrobe":WardrobeSave.query().fetch()
+        'user_nickname': user.nickname(),
+        'logoutUrl': users.create_logout_url('/'),
+        "topsWardrobe":WardrobeSave.query().fetch()
         }
         self.response.write(response_html.render(values))
 
@@ -88,10 +90,12 @@ class WardrobePage(webapp2.RequestHandler):
 
         response_html = jinja_env.get_template("templates/wardrobe_page.html")
         values = {
+            'user_nickname': user.nickname(),
+            'logoutUrl': users.create_logout_url('/'),
             "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False).fetch(),
             "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False).fetch(),
             "skirtWardrobe":WardrobeSave.query(WardrobeSave.type=="skirt", WardrobeSave.laundry==False).fetch(),
-            "dressWardrobe":WardrobeSave.query(WardrobeSave.type=="dress", WardrobeSave.laundry==False).fetch(),
+            "dressWardrobe":WardrobeSave.query(WardrobeSave.type=="dress", WardrobeSave.laundry==False).fetch()
         }
         self.response.write(response_html.render(values))
 
@@ -103,10 +107,12 @@ class FavoritesHandler(webapp2.RequestHandler):
 
         response_html = jinja_env.get_template("templates/addfavs_page.html")
         values = {
+            'user_nickname': user.nickname(),
+            'logoutUrl': users.create_logout_url('/'),
             "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False).fetch(),
             "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False).fetch(),
             "skirtWardrobe":WardrobeSave.query(WardrobeSave.type=="skirt", WardrobeSave.laundry==False).fetch(),
-            "dressWardrobe":WardrobeSave.query(WardrobeSave.type=="dress", WardrobeSave.laundry==False).fetch(),
+            "dressWardrobe":WardrobeSave.query(WardrobeSave.type=="dress", WardrobeSave.laundry==False).fetch()
         }
         self.response.write(response_html.render(values))
 
@@ -136,6 +142,8 @@ class GetWeather(webapp2.RequestHandler):
         }
         if (temp<35):
             values={
+                'user_nickname': user.nickname(),
+                'logoutUrl': users.create_logout_url('/'),
                 "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False).fetch(),
                 "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False, WardrobeSave.materials=="wool", WardrobeSave.materials=="denim", WardrobeSave.materials=="cotton", WardrobeSave.length=="long").fetch(),
                 "coatWardrobe":WardrobeSave.query(WardrobeSave.type=="coat", WardrobeSave.laundry==False).fetch(),
@@ -143,6 +151,8 @@ class GetWeather(webapp2.RequestHandler):
             }
         elif(temp>35 and temp<=50):
             values={
+                'user_nickname': user.nickname(),
+                'logoutUrl': users.create_logout_url('/'),
                 "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False).fetch(),
                 "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False, WardrobeSave.length=="long").fetch(),
                 "sweaterWardrobe":WardrobeSave.query(WardrobeSave.type=="sweater", WardrobeSave.laundry==False).fetch(),
@@ -150,17 +160,23 @@ class GetWeather(webapp2.RequestHandler):
             }
         elif(temp>50 and temp<=60):
             values={
+                'user_nickname': user.nickname(),
+                'logoutUrl': users.create_logout_url('/'),
                 "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False).fetch(),
                 "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False, WardrobeSave.length=="long").fetch(),
                 "sweaterWardrobe":WardrobeSave.query(WardrobeSave.type=="sweater", WardrobeSave.laundry==False).fetch()
             }
         elif(temp>60 and temp<=70):
             values={
+                'user_nickname': user.nickname(),
+                'logoutUrl': users.create_logout_url('/'),
                 "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False, WardrobeSave.length=="short").fetch(),
-                "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False, WardrobeSave.length=="long").fetch(),
+                "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False, WardrobeSave.length=="long").fetch()
             }
         else:
             values={
+                'user_nickname': user.nickname(),
+                'logoutUrl': users.create_logout_url('/'),
                 "topsWardrobe":WardrobeSave.query(WardrobeSave.type=="shirt", WardrobeSave.laundry==False, WardrobeSave.length=="short").fetch(),
                 "bottomWardrobe":WardrobeSave.query(WardrobeSave.type=="pants", WardrobeSave.laundry==False, WardrobeSave.length=="short").fetch(),
                 "skirtWardrobe":WardrobeSave.query(WardrobeSave.type=="skirt", WardrobeSave.laundry==False).fetch(),
@@ -172,10 +188,6 @@ class GetWeather(webapp2.RequestHandler):
 
 class TesterHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        logging.info('current user is: %s' % (user.nickname()))
-        response_html = jinja_env.get_template("templates/main_page.html")
-
         response_html = jinja_env.get_template("templates/weather-test.html")
         self.response.write(response_html.render())
         temp = self.request.get("temp")
