4eb286591e454b1a438c45d207c22c022f2e2634
Barbara <barbarac2339@gmail.com>
2018-07-25 15:57:35 -0500

    :/

--- WeatherOrNot/static/addfavs_page.js
+++ WeatherOrNot/static/addfavs_page.js
@@ -1,4 +1,3 @@
-var currentGifUrl = null;
 // Makes the element with ID 'resultPane' visible, and sets the element with ID
 // 'result' to contain resultJson
 
@@ -11,17 +10,27 @@ function addGifToFavorites(doneCallback) {
   var bottom= document.querySelector('#bottoms').value;
   jQuery.post("/list_favorite", {top, bottom}, doneCallback);
 }
-
+function addOutfitToHistory(doneCallback){
+  var top = document.querySelector('#tops').value;
+  var bottom= document.querySelector('#bottoms').value;
+  jQuery.post("/calendar", {top, bottom}, doneCallback);
+}
 // TODO: Create an event handler for when the button is clicked
 // that calls queryGiphy using the displayResult function as the callback
 
 function submitClick() {
   addGifToFavorites(() => {
     alert("saved")
-})
+  })
 }
 
+function woreClick(){
+  addOutfitToHistory(()=> {
+    alert('yay')
+  })
+}
 
 window.addEventListener('load', () => {
   document.querySelector('#submit').addEventListener("click", submitClick)
+  document.querySelector('#worn').addEventListener("click", woreClick)
 });
